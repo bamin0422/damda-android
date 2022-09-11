8f2f197ff379936a3b3f9e1e92ce7eda72e85d79
bamin0422 <bamin0422@gmail.com>
2022-09-12 02:01:09 +0900

    feat:settingsFragment 로그아웃 및 기능 구현

--- app/src/main/java/com/android/damda/ui/main/fragment/settings/SettingsFragment.kt
+++ app/src/main/java/com/android/damda/ui/main/fragment/settings/SettingsFragment.kt
@@ -4,22 +4,45 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.android.damda.R
 import com.android.damda.databinding.FragmentPhotoBinding
 import com.android.damda.databinding.FragmentSearchBinding
 import com.android.damda.databinding.FragmentSettingsBinding
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 
 class SettingsFragment : Fragment() {
     lateinit var binding : FragmentSettingsBinding
+    lateinit var userName : TextView
+    lateinit var userProfile : ImageView
+    lateinit var logoutBtn : Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+        initView(inflater, container)
         return binding.root
+    }
+
+    private fun initView(inflater: LayoutInflater, container: ViewGroup?) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+        userName = binding.userName
+        userProfile = binding.profileImg
+        logoutBtn = binding.logoutBtn
+
+        userName.text = FirebaseAuth.getInstance().currentUser?.displayName
+        Glide.with(this).load(FirebaseAuth.getInstance().currentUser?.photoUrl).into(userProfile)
+
+        logoutBtn.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            activity?.finish()
+        }
     }
 }
